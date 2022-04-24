1452f3196cb9ee2c669142f7e85811dc109d580e
YujiOYA <maxrahmen@gmail.com>
2022-04-25 03:40:31 +0900

    add next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "993c0bac70713a3c"; }
+/******/ 		__webpack_require__.h = function() { return "825e8ebd67621c68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
