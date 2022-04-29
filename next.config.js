eae739ceaf4bface9ecda44114b8e2b891283b59
YujiOYA <maxrahmen@gmail.com>
2022-04-29 16:17:07 +0900

    /

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const urlPrefix = process.env.GITHUB_ACTIONS? '/' + pf : ''
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
 
 module.exports = {
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
-  publicRuntimeConfig: { urlPrefix }, 
+  publicRuntimeConfig: { urlPrefix },  // ★コレ
 }
