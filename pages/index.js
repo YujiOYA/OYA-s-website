50cac7e75699effb95be0888187ec0dde8fe3fab
YujiOYA <maxrahmen@gmail.com>
2022-05-16 08:14:39 +0900

    slick image width 100%

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default ()=>{
       <div className="playbox">
         <div className="content">
           <div className="c_text">
-            html,css,JavaScript,PHP。
+            html,css,JavaScript,PHP
           </div>
         </div>
       </div>
