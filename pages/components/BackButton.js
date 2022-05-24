180d6772b973a7047013cc5e8ba597a951eaeffb
YujiOYA <90234957+YujiOYA@users.noreply.github.com>
2022-05-25 08:55:40 +0900

    Update BackButton.js
    export default

--- pages/components/BackButton.js
+++ pages/components/BackButton.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { Button } from "./button";
 
-export const BackButton=()=>{
+export default const BackButton=()=>{
     const router = useRouter();
     return(
         <Button alia-label="戻る" onClick={()=>router.push('/')}> ホームに戻る</Button>
