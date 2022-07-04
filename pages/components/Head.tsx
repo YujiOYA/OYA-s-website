1e64ec50afe686b5d8839e9fe8008f61138867e9
YujiOYA <maxrahmen@gmail.com>
2022-07-04 22:26:18 +0900

    favicon

--- pages/components/Head.tsx
+++ pages/components/Head.tsx
@@ -20,6 +20,29 @@ const IndexHead = (props:Props) => {
       <meta httpEquiv="imagetoolbar" content="no" />
 
       <meta charSet="utf-8" />
+      <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/325/sake_1f376.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/325/sake_1f376.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/325/sake_1f376.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
       <title>{props.title}</title>
     </Head>
   )
